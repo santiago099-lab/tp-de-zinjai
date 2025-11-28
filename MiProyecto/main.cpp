bc4f83f44841407e3e1e7c539d42909630e89d50
santiago099-lab <australiaelmejorpais099@gmail.com>
2025-11-27 23:13:27 -0300

    commit
    era una prueba para ver si me guardaba los commits

--- MiProyecto/main.cpp
+++ MiProyecto/main.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<windows.h>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
+const int ANCHO_PANTALLA = 80;
 int main (int argc, char *argv[]) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	return 0;
 }
